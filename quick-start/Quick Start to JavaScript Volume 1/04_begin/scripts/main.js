82271749e25944ef5903923b17e0cdee189d6447
julia schaefermeyer <julia.schaefermeyer@gmail.com>
2017-01-31 19:02:35 -0700

    Initial verstion to github

---
+++ quick-start/Quick Start to JavaScript Volume 1/04_begin/scripts/main.js
@@ -0,0 +1,37 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+3+3;
+/*
+6
+*/
+5-4;
+/*
+1
+*/
+5*5;
+/*
+25
+*/
+22/5;
+/*
+4.4
+*/
+22%5;
+/*
+2
+*/
+34-2*3;
+/*
+28
+*/
+(34-2)*3;
+/*
+96
+*/
